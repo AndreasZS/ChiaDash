73af18dfb1fc8ee9f7398a398c30b2d6845bf1bb
AndreasZS <AndreasZS@github.com>
2021-09-05 15:07:31 -0400

    Added Quit button. Wrote notes in comments during call w/ Greg.

--- client/gui.py
+++ client/gui.py
@@ -32,10 +32,30 @@ class App(tk.Frame):
               self.contents.get())
 
 
-
+# Create Toplevel widget as root
 root = tk.Tk()
+root.geometry("500x300")
+root.configure(bg='#454648')
+# Create frame widget with root as background
 myapp = App(root)
 
+# Set Toplevel title
 myapp.master.title("My Do-Nothing Application")
-
+# Create 'Quit' button
+tk.Button(myapp, text="Quit", command=root.destroy).pack()
+# Create print_contents button
+tk.Button(myapp, text="Show Contents", command=myapp.print_contents(1)).pack()
+# Call mainloop of Tk
 myapp.mainloop()
+"""
+Run Chia, specify number of plots you want
+Greg uses a plot manager, configures it to use all the drives you have and press go
+It selects drives and plots, spins them up in parallel
+swar plot manager
+plotting generates log files, Chia and swar have log files
+Parsing log files provides information
+Lots of different statuses provided from log files
+Raspberry Pi - make webserver, have page that can be accessed from personal computer e.g. display time or whatever
+Have personal pc parse the log then send back relavent data e.g. how many plots are done
+Every Chia harvester has debug log
+1 computer hosts full node w/ copy of full blockchain. Each server is a Chia harvester"""
